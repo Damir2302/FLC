25149a64491e261b081c0108ec4da90d601bb0c7
Nicholas <mykolakratasyk@gmail.com>
2023-02-06 17:18:49 +0300

    start what we do section

--- src/assets/js/slider.js
+++ src/assets/js/slider.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
 
+    
+    let whatWeDoSlider = new Swiper('.wwdo__items', {
+        slidesPerView: 1,
+        spaceBetween: 25,
+        pagination: {
+            el: '.wwde__pagination',
+            clickable: true,
+           
+        },
+        breakpoints: {
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            },
+            744: {
+                slidesPerView: 2,
+                spaceBetween: 30
+            },
+        }
+    });
 
 })

275c5359960fe60891250b074c0610d4aa660a93
Nicholas <mykolakratasyk@gmail.com>
2023-02-07 10:12:23 +0300

    end what we do section

--- src/assets/js/slider.js
+++ src/assets/js/slider.js
@@ -1,24 +1,35 @@
 $(document).ready(function() {
 
     
-    let whatWeDoSlider = new Swiper('.wwdo__items', {
-        slidesPerView: 1,
-        spaceBetween: 25,
-        pagination: {
-            el: '.wwde__pagination',
-            clickable: true,
-           
-        },
-        breakpoints: {
-            1024: {
-                slidesPerView: 3,
-                spaceBetween: 30
+    if ($(window).width() < 1400) {
+        let whatWeDoSlider = new Swiper('.wwdo__items', {
+            slidesPerView: 1,
+            spaceBetween: 25,
+            pagination: {
+                el: '.wwde__pagination',
+                clickable: true,
+               
             },
-            744: {
-                slidesPerView: 2,
-                spaceBetween: 30
-            },
-        }
-    });
+            breakpoints: {
+                1400: {
+                    slidesPerView: 6,
+                    spaceBetween: 35
+                },
+                1024: {
+                    slidesPerView: 4,
+                    spaceBetween: 35
+                },
+                744: {
+                    slidesPerView: 2,
+                    spaceBetween: 30,
+                    watchSlidesVisibility: true,
+                    watchOverflow: true,
+                },
+            }
+        });
+    }
+
+
+
 
 })

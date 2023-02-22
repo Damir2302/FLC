958c66f03dfd16f211a3da7007c2d0b31ccca00e
Damir S <damir23.02@mail.ru>
2023-02-22 16:17:51 +0300

    fixed

---
+++ www/assets/js/main.js
@@ -0,0 +1,202 @@
+$(document).ready(function () {
+
+  // $('input[type="tel"]').inputmask({"mask": "+7 (999) 99-99-99", 'showMaskOnHover': false})
+
+  // HEDAER BURGER MENU
+  $(".header__burger").on("click", function () {
+    if ($(this).hasClass("opened")) {
+      $("body").removeClass("overflow-hidden");
+
+      $(this).removeClass("opened");
+      $(".navbar").removeClass("opened");
+    } else {
+      $("body").addClass("overflow-hidden");
+
+      $(this).addClass("opened");
+      $(".navbar").addClass("opened");
+    }
+  });
+
+  $(".nav__item.has-child .arrow-down").on("click", function () {
+    $(this).parent().toggleClass("opened");
+    $(this).parent().find(".nav__submenu").slideToggle();
+  });
+
+  // Show the first tab by default
+  if(!$(".tabs__content").hasClass("custom-tab")) {
+    $(".tabs__content .tabs__content-item").hide();
+    $(".tabs__content .tabs__content-item:first").show();
+  }
+
+  $("#tab-4").hide();
+
+  $(".tabs__nav .tabs__nav-item:first").addClass("tab--active");
+
+  // Change tab class and display content
+  $(".tabs__nav-item a").on("click", function (event) {
+
+    $(".tabs__nav .tabs__nav-item").removeClass("tab--active");
+    $(this).parent().addClass("tab--active");
+
+    if(!$(".tabs__content").hasClass("custom-tab")) {
+      event.preventDefault();
+      $(".tabs__content .tabs__content-item").hide();
+      $($(this).attr("href")).show();
+    } else {
+
+      if ($(this).attr("href") == "#tab-4") {
+        event.preventDefault();
+        $(".tabs__content .tabs__content-item").hide();
+        $($(this).attr("href")).show();
+      } else {
+        $(".tabs__content .tabs__content-item").show();
+        $("#tab-4").hide();
+      }
+    }
+
+  });
+
+  $(".js-faq-title").on("click", function (e) {
+    e.preventDefault();
+    var $this = $(this);
+
+    if (!$this.hasClass("faq__active")) {
+      $(".js-faq-content").slideUp(800);
+      $(".js-faq-title").removeClass("faq__active");
+      $(".js-faq-rotate").removeClass("faq__rotate");
+    }
+
+    $this.toggleClass("faq__active");
+    $this.next().slideToggle();
+    $(".js-faq-rotate", this).toggleClass("faq__rotate");
+  });
+
+  // Диаграмма на главной
+  $(".results__btn").on("click", function () {
+    $(this).parent().find(".results__info").slideDown();
+    $(this).css("opacity", 0);
+  });
+
+  $(".results__info-btn").on("click", function () {
+    $(this).parent().parent().find(".results__info").slideUp();
+    $(this).parent().parent().find(".results__btn").css("opacity", 1);
+  });
+
+  if ($(".results-section").length) {
+    $(document).scroll(function () {
+      if ($(this).scrollTop() >= $(".results-section").offset().top - 500) {
+        $(".results-section").addClass("focused").trigger("classChange");
+      }
+    });
+  }
+
+  $(".results-section").on("classChange", function () {
+    diagramStats();
+  });
+
+  function diagramStats() {
+    if ($(".results__diagram-value span").text() == 0) {
+      $(".results__diagram-value").each(function (e) {
+        let _that = $(this);
+        let num = 0;
+
+        if (_that.attr("data-value") != 0) {
+          const statsCount = setInterval(function () {
+            num++;
+
+            _that.find("span").text(num);
+
+            if (_that.find("span").text() == _that.attr("data-value")) {
+              clearInterval(statsCount);
+            }
+          }, 50);
+        }
+      });
+    }
+  }
+
+  if ($(window).width() >= 744) {
+    $(".results__item").on("mouseenter", function (e) {
+      let rect = e.target.getBoundingClientRect();
+      let x = e.clientX - rect.left; //x position within the element.
+      let y = e.clientY - rect.top; //y position within the element.
+
+      $(this).find(".results__info").css("display", "block");
+      $(this).find(".results__info").css("left", x);
+      $(this).find(".results__info").css("top", y);
+    });
+
+    $(".results__item").on("mouseleave", function () {
+      $(this).find(".results__info").css("display", "none");
+    });
+  }
+
+    // 3 Варианта оплаты на странице Судебное представительство
+    $('.payments__var-block').on('click', function() {
+
+        if (!$(this).hasClass('opened')) {
+            $('.payments__var-block').removeClass('opened')
+            $(this).addClass('opened')
+            $('.payments__var-text').slideUp()
+            $(this).find('.payments__var-text').slideDown()
+        } else {
+            $(this).removeClass('opened')
+            $(this).find('.payments__var-text').slideUp()
+        }
+    })
+
+
+  // ACCORDION MOBILE
+  $(function () {
+    // (Optional) Active an item if it has the class "is-active"
+    $(".acc__card.is-active").children(".acc__panel").slideDown();
+
+    $(".acc__title ").click(function () {
+      // Cancel the siblings
+      $(this)
+        .parent()
+        .siblings(".acc__card")
+        .removeClass("is-active")
+        .children(".acc__panel")
+        .slideUp();
+      // Toggle the item
+      $(this)
+        .parent()
+        .toggleClass("is-active")
+        .find(".acc__panel")
+        .slideToggle("ease-out");
+    });
+  });
+
+  // ACCORDION DESC
+
+  $(function () {
+    $(".acc__desc-nav a").click(function () {
+      // Check for active
+
+      $(this)
+        .parent()
+        .parent()
+        .find(".acc__desc-nav-item")
+        .removeClass("active");
+      $(this).addClass("active");
+
+      // Display active tab
+      let currentTab = $(this).attr("href");
+      $(this)
+        .parent()
+        .parent()
+        .find(".acc__desc-content .acc__desc-tab")
+        .hide();
+      $(currentTab).show();
+
+      return false;
+    });
+  });
+
+  $(function () {
+    $(".acc__get-cost-action").click(function () {
+      $(this).next().slideToggle();
+    });
+  });
+});
---
+++ www/assets/js/slider.js
@@ -0,0 +1,577 @@
+$(document).ready(function() {
+
+
+    if ($(window).width() < 1400) {
+        let whatWeDoSlider = new Swiper('.wwdo__items', {
+            slidesPerView: 1,
+            spaceBetween: 25,
+            pagination: {
+                el: '.wwde__pagination',
+                clickable: true,
+
+            },
+            breakpoints: {
+                1400: {
+                    slidesPerView: 6,
+                    spaceBetween: 35
+                },
+                1024: {
+                    slidesPerView: 4,
+                    spaceBetween: 35
+                },
+                744: {
+                    slidesPerView: 2,
+                    spaceBetween: 30,
+                    watchSlidesVisibility: true,
+                    watchOverflow: true,
+                },
+            }
+        });
+    }
+
+
+    let outsourcingOptionsSlider = new Swiper('.outsourcing-options__items', {
+        slidesPerView: 1,
+        spaceBetween: 35,
+
+        navigation: {
+            nextEl: ".outsourcing-options__nav-next",
+            prevEl: ".outsourcing-options__nav-prev",
+        },
+
+        pagination: {
+            el: '.outsourcing-options__pagination',
+            clickable: true,
+            renderBullet: function (index, className) {
+                return '<span class="' + className + '"><div>' + (index + 1) + "</div></span>";
+            },
+        },
+
+        breakpoints: {
+            744: {
+              slidesPerView: 'auto',
+              spaceBetween: 20,
+              centeredSlides: true,
+              loop: true,
+            }
+        },
+
+
+    });
+
+    // ТАБЫ
+    let tabsNavSlider;
+    let inittabsNavSlider;
+
+    tabsNavSliderCheck();
+
+    $(window).on('resize', function() {
+      tabsNavSliderCheck()
+    })
+
+    function tabsNavSliderCheck() {
+
+      if ($('.tabs__nav').length) {
+
+        if ($(window).width() < 1400) {
+          if (!inittabsNavSlider) {
+            inittabsNavSlider = true;
+            tabsNavSlider = new Swiper(".tabs__nav", {
+              slidesPerView: 'auto',
+              freeMode: true
+            })
+          }
+        } else {
+          if (typeof(tabsNavSlider) !== "undefined" ) {
+            inittabsNavSlider = false;
+            tabsNavSlider.destroy();
+          }
+        }
+      }
+    }
+
+    let workMetodsSldier = new Swiper('.work-metods__items', {
+        slidesPerView: 'auto',
+        watchOverflow: true,
+        centeredSlides: true,
+        loop: true,
+        autoHeight: true,
+        spaceBetween: 55,
+        navigation: {
+            nextEl: ".work-metods__nav-next",
+            prevEl: ".work-metods__nav-prev",
+        },
+
+        pagination: {
+            el: '.work-metods__pagination',
+            clickable: true,
+        },
+
+    });
+
+
+    let reviewsSlider = new Swiper('.reviews__items', {
+        slidesPerView: 1,
+        spaceBetween: 35,
+
+        navigation: {
+            nextEl: ".reviews__items-nav-next",
+            prevEl: ".reviews__items-nav-prev",
+        },
+
+        pagination: {
+            el: '.reviews__pagination',
+            clickable: true,
+        },
+
+        breakpoints: {
+            // 1400: {
+            //     slidesPerView: 6,
+            //     spaceBetween: 35
+            // },
+            // 1024: {
+            //     slidesPerView: 4,
+            //     spaceBetween: 35
+            // },
+            744: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+                watchSlidesVisibility: true,
+                watchOverflow: true,
+            },
+        }
+    });
+
+    // Диаграмма на главной
+    let resultsSlider;
+    let initResultsSlider;
+
+    resultsSliderCheck();
+
+    $(window).on('resize', function() {
+        resultsSliderCheck()
+    })
+
+    function resultsSliderCheck() {
+
+      if ($('.results .swiper').length) {
+
+        if ($(window).width() < 744) {
+          if (!initResultsSlider) {
+            initResultsSlider = true;
+            resultsSlider = new Swiper(".results .swiper", {
+              slidesPerView: 1,
+              spaceBetween: 30,
+
+              pagination: {
+                el: '.results .swiper-pagination',
+                clickable: true
+              }
+            })
+          }
+        } else {
+          if (typeof(resultsSlider) !== "undefined" ) {
+            initResultsSlider = false;
+            resultsSlider.destroy();
+          }
+        }
+      }
+    }
+
+    // Команда FLC
+    let teamSlider;
+    let initTeamSlider;
+
+    teamSliderCheck();
+
+    $(window).on('resize', function() {
+        teamSliderCheck()
+    })
+
+    function teamSliderCheck() {
+
+      if ($('.team .swiper').length) {
+
+        if ($(window).width() < 744) {
+          if (!initTeamSlider) {
+            initTeamSlider = true;
+            resultsSlider = new Swiper(".team .swiper", {
+              slidesPerView: 1,
+              spaceBetween: 30,
+
+              pagination: {
+                el: '.team .swiper-pagination',
+                clickable: true
+              }
+            })
+          }
+        } else {
+          if (typeof(resultsSlider) !== "undefined" ) {
+            initTeamSlider = false;
+            resultsSlider.destroy();
+          }
+        }
+      }
+    }
+
+    // Сферы бизнеса
+    let expSlider;
+    let initExpSlider;
+
+    expSliderCheck();
+
+    $(window).on('resize', function() {
+        expSliderCheck()
+    })
+
+    function expSliderCheck() {
+
+      if ($('.experience .swiper').length) {
+
+        if ($(window).width() < 744) {
+          if (!initExpSlider) {
+            initExpSlider = true;
+            expSlider = new Swiper(".experience .swiper", {
+              slidesPerView: 2,
+              spaceBetween: 10,
+
+              pagination: {
+                el: '.experience .swiper-pagination',
+                clickable: true
+              }
+            })
+          }
+        } else {
+          if (typeof(expSlider) !== "undefined" ) {
+            initExpSlider = false;
+            expSlider.destroy();
+          }
+        }
+      }
+    }
+
+    // Налоговый план
+    let planSlider;
+    let initPlanSlider;
+
+    planSliderCheck();
+
+    $(window).on('resize', function() {
+      planSliderCheck()
+    })
+
+    function planSliderCheck() {
+
+      if ($('.t-planning__slider .swiper').length) {
+
+        if ($(window).width() < 744) {
+          if (!initPlanSlider) {
+            initPlanSlider = true;
+            planSlider = new Swiper(".t-planning__slider .swiper", {
+              slidesPerView: 1,
+              spaceBetween: 30,
+
+              pagination: {
+                el: '.t-planning__slider .swiper-pagination',
+                clickable: true
+              }
+            })
+          }
+        } else {
+          if (typeof(planSlider) !== "undefined" ) {
+            initPlanSlider = false;
+            planSlider.destroy();
+          }
+        }
+      }
+    }
+
+    let casesSlider = new Swiper('.cases__items', {
+        slidesPerView: 'auto',
+        watchOverflow: true,
+        centeredSlides: true,
+        loop: false,
+        autoHeight: true,
+        spaceBetween: 55,
+        navigation: {
+            nextEl: ".cases__nav-next",
+            prevEl: ".cases__nav-prev",
+        },
+        pagination: {
+            el: '.cases__pagination',
+            clickable: true,
+        },
+    });
+
+    let trustUsSlider = new Swiper('.trust-us__slider', {
+        slidesPerView: 2,
+        spaceBetween: 25,
+
+        navigation: {
+            nextEl: ".trust-us__nav-next",
+            prevEl: ".trust-us__nav-prev",
+        },
+
+        pagination: {
+            el: '.trust-us__pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            1400: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+                grid: {
+                    rows: 3,
+                    fill: "row"
+                }
+            },
+            1024: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+                grid: {
+                    rows: 2,
+                    fill: "row"
+                }
+            },
+            744: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+                grid: {
+                    rows: 2,
+                    fill: "row"
+                },
+            },
+        }
+
+    });
+
+    let trustUsOnMainSlider = new Swiper('.trust-us-slider', {
+      slidesPerView: 2,
+      spaceBetween: 10,
+
+      navigation: {
+          nextEl: ".trust-us__nav-next",
+          prevEl: ".trust-us__nav-prev",
+      },
+
+      pagination: {
+          el: '.trust-us__pagination',
+          clickable: true,
+      },
+
+      breakpoints: {
+          744: {
+              slidesPerView: 4,
+              spaceBetween: 10
+          },
+
+          1400: {
+            slidesPerView: 5,
+            spaceBetween: 20
+          }
+      }
+
+    });
+
+    // Виды судебных споров
+    let litigationSlider;
+    let initLitigationSlider;
+
+    litigationSliderCheck();
+
+    $(window).on('resize', function() {
+      litigationSliderCheck()
+    })
+
+    function litigationSliderCheck() {
+
+      if ($('.litigation .swiper').length) {
+
+        if ($(window).width() < 1024) {
+          if (!initLitigationSlider) {
+            initLitigationSlider = true;
+            litigationSlider = new Swiper(".litigation .swiper", {
+              slidesPerView: 1,
+              spaceBetween: 30,
+
+              pagination: {
+                el: '.litigation .swiper-pagination',
+                clickable: true
+              },
+
+              breakpoints: {
+                744: {
+                  slidesPerView: 2,
+                  spaceBetween: 30,
+                }
+              }
+            })
+          }
+        } else {
+          if (typeof(litigationSlider) !== "undefined" ) {
+            initLitigationSlider = false;
+            litigationSlider.destroy();
+          }
+        }
+      }
+    }
+
+    //  Команда судебных юристов на странице Судебное представительство
+    let tLawyer = new Swiper('.t-lawyer .swiper', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      autoplay: {
+        delay: 10000,
+        disableOnInteraction: false
+      },
+
+      pagination: {
+          el: '.t-lawyer .swiper-pagination',
+          clickable: true,
+      },
+
+      breakpoints: {
+        744: {
+          pagination: {
+            renderBullet: function (index, className) {
+              let imageContainer = [];
+              $('.t-lawyer__image').each(function(e) {
+                imageContainer.push($('.t-lawyer__image').html())
+              })
+
+              return '<div class="' + className + '">' + imageContainer[index] + `<svg>
+                      <circle cx="30" cy="30" r="30"></circle>
+                      <circle class="progress-bar" id="progress-bar" cx="30" cy="30" r="30"></circle></svg>`  + '</div>';
+            }
+          }
+        },
+
+        1024: {
+          slidesPerView: 1,
+          spaceBetween: 40,
+        },
+
+        1400: {
+          slidesPerView: 1,
+          spaceBetween: 100,
+
+          pagination: {
+            renderBullet: function (index, className) {
+              let imageContainer = [];
+              $('.t-lawyer__image').each(function(e) {
+                imageContainer.push($('.t-lawyer__image').html())
+              })
+
+              return '<div class="' + className + '">' + imageContainer[index] + `<svg>
+                      <circle cx="30" cy="30" r="30"></circle>
+                      <circle class="progress-bar" id="progress-bar" cx="30" cy="30" r="30"></circle></svg>`  + '</div>';
+            }
+          }
+        }
+      }
+
+    });
+
+    let compareSlider = new Swiper('.compare__items', {
+        slidesPerView: 1,
+        // watchOverflow: true,
+        // centeredSlides: true,
+        loop: false,
+        // autoHeight: true,
+        spaceBetween: 55,
+
+        pagination: {
+            el: '.compare__pagination',
+            clickable: true,
+        },
+
+        breakpoints: {
+            1400: {
+                slidesPerView: 2,
+                spaceBetween: 60,
+            },
+            1024: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            744: {
+                slidesPerView: 2,
+                spaceBetween: 10,
+            },
+        }
+    });
+
+
+    let variantsSldier = new Swiper('.tab__variants', {
+      slidesPerView: 1,
+      // watchOverflow: true,
+      // centeredSlides: true,
+      loop: false,
+      // autoHeight: true,
+      spaceBetween: 55,
+
+      pagination: {
+          el: '.tab__variants-pagination',
+          clickable: true,
+      },
+
+      breakpoints: {
+
+          1024: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+          },
+          744: {
+              slidesPerView: 2,
+              spaceBetween: 10,
+          },
+      }
+  });
+
+
+
+  let auditResultSldier = new Swiper('.prav-audit-result__items', {
+    slidesPerView: 1,
+    // watchOverflow: true,
+    // centeredSlides: true,
+    loop: false,
+    // autoHeight: true,
+    spaceBetween: 55,
+
+    pagination: {
+        el: ' .prav-audit-result__pagination',
+        clickable: true,
+    },
+
+    breakpoints: {
+
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+        744: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+    }
+});
+
+if ($(window).width() < 744) {
+  let workRulesSlider = new Swiper('.work-rules', {
+    slidesPerView: 1,
+    // watchOverflow: true,
+    // centeredSlides: true,
+    loop: false,
+    // autoHeight: true,
+    spaceBetween: 55,
+
+    pagination: {
+        el: ' .work-rules__pagination',
+        clickable: true,
+    },
+
+  });
+}
+
+
+})
